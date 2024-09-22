111eeaef4f5adb91266be5cd4be57513618bf271
subomega1 <ahmedkhalil.sfar@etudiant-fst.utm.tn>
2024-09-22 14:07:20 +0100

    2D

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -1,6 +1,7 @@
 package Main;
 
 import entity.Player;
+import tile.TileManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,20 +12,21 @@ public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16;//16x16
     final int scale = 3;
     public final int tileSize = originalTileSize * scale;//48x48
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize*maxScreenCol;//768
-    final int screenHeight = tileSize*maxScreenRow;//576
+   public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize*maxScreenCol;//768
+    public final int screenHeight = tileSize*maxScreenRow;//576
     //FPS = 60
     int FPS = 60;
+    TileManager tileM = new TileManager(this);
 
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     Player  player = new Player(this , keyH);
     //set original position
-    int playerX =100;
-    int playerY = 100;
-    int playerSpeed = 4;
+    //int playerX =100;
+    //int playerY = 100;
+   // int playerSpeed = 4;
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
@@ -68,6 +70,7 @@ public class GamePanel extends JPanel implements Runnable {
     public void paintComponent( Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
+        tileM.draw(g2);
         player.draw(g2);
         g2.dispose();
 
